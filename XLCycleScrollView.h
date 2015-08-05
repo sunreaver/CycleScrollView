64f6175ac3b95c65b7cfffbcfd94222b84832eae
tanwei <tanweirush@users.noreply.github.com>
2015-08-05 10:10:39 +0800

    Update XLCycleScrollView.h
    修改，使适应XCode6

--- XLCycleScrollView.h
+++ XLCycleScrollView.h
@@ -12,22 +12,10 @@
 @protocol XLCycleScrollViewDatasource;
 
 @interface XLCycleScrollView : UIView<UIScrollViewDelegate>
-{
-    UIScrollView *_scrollView;
-    UIPageControl *_pageControl;
-    
-    id<XLCycleScrollViewDelegate> _delegate;
-    id<XLCycleScrollViewDatasource> _datasource;
-    
-    NSInteger _totalPages;
-    NSInteger _curPage;
-    
-    NSMutableArray *_curViews;
-}
 
 @property (nonatomic,readonly) UIScrollView *scrollView;
 @property (nonatomic,readonly) UIPageControl *pageControl;
-@property (nonatomic,assign) NSInteger currentPage;
+@property (nonatomic,assign) NSInteger curPage;
 @property (nonatomic,assign,setter = setDataource:) id<XLCycleScrollViewDatasource> datasource;
 @property (nonatomic,assign,setter = setDelegate:) id<XLCycleScrollViewDelegate> delegate;
 
